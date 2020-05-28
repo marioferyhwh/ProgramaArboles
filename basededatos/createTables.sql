5fe64bbda9794fc99d5f7dba4f380bc55009037f
mario robayo <marioferyhwh@gmail.com>
2020-05-28 14:06:00 -0500

    tabla collection (prestamos)

--- basededatos/createTables.sql
+++ basededatos/createTables.sql
@@ -1,7 +1,23 @@
 
 
-CREATE ROLE arbol WITH SUPERUSER LOGIN ENCRYPTED PASSWORD 'arboles';
-CREATE DATABASE arboles OWNER arbol;
+CREATE ROLE arbol
+WITH SUPERUSER LOGIN ENCRYPTED PASSWORD 'arboles';
+CREATE DATABASE arboles
+OWNER arbol;
 
--- psql -d arbol -U arbol
+-- psql -d arboles -U arbol
 -- arboles
+
+DROP TABLE arboles.Collectiones
+
+CREATE TABLE arboles.Collectiones
+(
+  id serial NOT NULL ,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp,
+  delete_at timestamp,
+  descrip varchar(256) DEFAULT '',
+  active bool NOT NULL DEFAULT TRUE,
+  balance_total numeric(7,1) NOT NULL DEFAULT 0,
+  CONSTRAINT pk_Collectiones PRIMARY KEY(id)
+);
