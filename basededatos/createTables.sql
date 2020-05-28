5f6bfc5d066549c18f7b94a37124105c25093b04
mario robayo <marioferyhwh@gmail.com>
2020-05-28 14:11:02 -0500

    table user levels

--- basededatos/createTables.sql
+++ basededatos/createTables.sql
@@ -8,7 +8,7 @@ OWNER arbol;
 -- psql -d arboles -U arbol
 -- arboles
 
-DROP TABLE arboles.Collectiones
+DROP TABLE arboles.Collectiones;
 
 CREATE TABLE arboles.Collectiones
 (
@@ -20,4 +20,17 @@ CREATE TABLE arboles.Collectiones
   active bool NOT NULL DEFAULT TRUE,
   balance_total numeric(7,1) NOT NULL DEFAULT 0,
   CONSTRAINT pk_Collectiones PRIMARY KEY(id)
+);
+
+
+DROP TABLE arboles.UserLevels;
+
+CREATE TABLE arboles.UserLevels
+(
+  id smallserial NOT NULL ,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp,
+  delete_at timestamp,
+  level varchar(11) DEFAULT '',
+  CONSTRAINT pk_UserLevels PRIMARY KEY(id)
 );
