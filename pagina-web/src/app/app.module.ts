60c2c804a1b6c5abf13ce675c7d8030a0529ba22
mario robayo <marioferyhwh@gmail.com>
2020-06-15 13:45:30 -0500

    se eliminan los modulos  y se crea formulario d elogin

--- pagina-web/src/app/app.module.ts
+++ pagina-web/src/app/app.module.ts
@@ -1,22 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ComponentsModule } from './modules/components/components.module';
-import { ModelsModule } from './shared/models/models.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { HeaderComponent } from "./modules/components/header/header.component";
+import { FooterComponent } from "./modules/components/footer/footer.component";
+import { ButtonComponent } from "./modules/components/button/button.component";
+import { TableClientComponent } from "./modules/components/table-client/table-client.component";
+import { TableCollectionComponent } from "./modules/components/table-collection/table-collection.component";
+import { TableLoanComponent } from "./modules/components/table-loan/table-loan.component";
+import { TableLoanPaymentComponent } from "./modules/components/table-loan-payment/table-loan-payment.component";
+import { TableExpenseComponent } from "./modules/components/table-expense/table-expense.component";
+import { TableCashComponent } from "./modules/components/table-cash/table-cash.component";
+import { FormUserComponent } from "./modules/components/form-user/form-user.component";
+import { FormClienteComponent } from "./modules/components/form-cliente/form-cliente.component";
+import { FormCollectionComponent } from "./modules/components/form-collection/form-collection.component";
+import { FormLoanComponent } from "./modules/components/form-loan/form-loan.component";
+import { FormLoanPaymentComponent } from "./modules/components/form-loan-payment/form-loan-payment.component";
+import { FormExpenseComponent } from "./modules/components/form-expense/form-expense.component";
+import { FormGeneralComponent } from "./modules/components/form-general/form-general.component";
+import { FormTelComponent } from "./modules/components/form-tel/form-tel.component";
+import { FormLocationComponent } from "./modules/components/form-location/form-location.component";
+import { FormCashComponent } from "./modules/components/form-cash/form-cash.component";
+import { FormLoginComponent } from "./modules/components/form-login/form-login.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsModule,
-    ModelsModule
+    //ComponentsModule,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    TableClientComponent,
+    TableCollectionComponent,
+    TableLoanComponent,
+    TableLoanPaymentComponent,
+    TableExpenseComponent,
+    TableCashComponent,
+    FormUserComponent,
+    FormClienteComponent,
+    FormCollectionComponent,
+    FormLoanComponent,
+    FormLoanPaymentComponent,
+    FormExpenseComponent,
+    FormGeneralComponent,
+    FormTelComponent,
+    FormLocationComponent,
+    FormCashComponent,
+    FormLoginComponent,
+    //ModelsModule,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, FormsModule],
 })
-export class AppModule { }
+export class AppModule {}
