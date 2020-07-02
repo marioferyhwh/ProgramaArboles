655c7f847584d590733cc4bb9b7deb12108187a0
mario robayo <marioferyhwh@gmail.com>
2020-07-02 14:58:06 -0500

    se agrea la opcionde agregar telñefonos n

--- pagina-web/src/app/modules/components/form-client/form-client.component.ts
+++ pagina-web/src/app/modules/components/form-client/form-client.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { ClientModel } from "src/app/shared/models/client.model";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators, FormArray } from "@angular/forms";
 import { GlobalService } from "src/app/services/global.service";
 import { DocumentTypeModel } from "src/app/shared/models/document-type.model";
 import { LoanStateModel } from "src/app/shared/models/loan-state.model";
 import { TelDescriptionModel } from "src/app/shared/models/tel-description.model";
 import { BusinessTypeModel } from "src/app/shared/models/business-type.model";
+import { isUndefined } from "util";
 
 @Component({
   selector: "app-form-client",
@@ -20,9 +21,10 @@ export class FormClienteComponent implements OnInit {
   public documents: DocumentTypeModel[];
   public loan_states: LoanStateModel[];
   public businesstype: BusinessTypeModel[];
-  public tels: TelDescriptionModel[];
+  public telds: TelDescriptionModel[];
   constructor(private _globalService: GlobalService, private _fb: FormBuilder) {
-    this.initform();
+    this.initForm();
+    this.upData();
   }
 
   ngOnInit(): void {}
@@ -36,17 +38,17 @@ export class FormClienteComponent implements OnInit {
       return;
     }
     console.log(this.client);
-    console.log(this.forma["controls"]);
+    console.log(this.forma.value);
     this.clientData.emit(this.client);
   }
 
-  initform() {
+  initForm() {
     this._globalService.get().subscribe((dt) => {
       console.log({ dt });
       this.documents = dt.document_types;
       this.loan_states = dt.loan_states;
       this.businesstype = dt.business_types;
-      this.tels = dt.tel_descriptions;
+      this.telds = dt.tel_descriptions;
       this.documents.unshift({
         description: "-- selecione tipo de documento --",
       });
@@ -54,7 +56,7 @@ export class FormClienteComponent implements OnInit {
         //id: 0,
         type_business: "-- selecione negocio --",
       });
-      this.tels.unshift({
+      this.telds.unshift({
         //id: 0,
         tel_description: "-- selecione  descripcion --",
       });
@@ -69,41 +71,69 @@ export class FormClienteComponent implements OnInit {
       id_loan_state: ["", [Validators.required]],
       id_type_business: ["", [Validators.required]],
       id_location: ["", [Validators.required]],
+      tels: this._fb.array([]),
+
       //id_collection: [""],
       //id_user: [""],
     });
   }
 
-  nameInvalid(): boolean {
+  addTel() {
+    this.telsArray.push(
+      this._fb.group({
+        id_tel_descript: [1, Validators.required],
+        number: ["", Validators.required],
+      })
+    );
+  }
+
+  removeTel(i: number) {
+    this.telsArray.removeAt(i);
+  }
+
+  get telsArray(): FormArray {
+    return <FormArray>this.forma.get("tels");
+  }
+
+  numberInvalid(i: number) {}
+
+  get nameInvalid(): boolean {
     return this.forma.get("name").invalid && this.forma.get("name").touched;
   }
 
-  emailInvalid(): boolean {
+  get emailInvalid(): boolean {
     return this.forma.get("email").invalid && this.forma.get("email").touched;
   }
 
-  adressInvalid(): boolean {
+  get adressInvalid(): boolean {
     return this.forma.get("adress").invalid && this.forma.get("adress").touched;
   }
 
-  id_loan_stateInvalid(): boolean {
+  get id_loan_stateInvalid(): boolean {
     return (
       this.forma.get("id_loan_state").invalid &&
       this.forma.get("id_loan_state").touched
     );
   }
 
-  id_type_businessInvalid(): boolean {
+  get id_type_businessInvalid(): boolean {
     return (
       this.forma.get("id_type_business").invalid &&
       this.forma.get("id_type_business").touched
     );
   }
 
-  id_locationInvalid(): boolean {
+  get id_locationInvalid(): boolean {
     return (
       this.forma.get("id_location").invalid &&
       this.forma.get("id_location").touched
     );
+  }
+
+  upData() {
+    if (this.client != null) {
+      // this.forma.setValue(this.client);
+      this.forma.reset(this.client);
+    }
   }
 }
