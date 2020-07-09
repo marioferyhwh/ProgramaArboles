1721e48068fb8a542c1e8362a569fcde428c4f5c
mario robayo <marioferyhwh@gmail.com>
2020-07-08 20:05:11 -0500

    se agrega el boton de  nuevo en  las listas

--- pagina-web/src/app/modules/components/table-user/table-user.component.ts
+++ pagina-web/src/app/modules/components/table-user/table-user.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { UserModel } from "src/app/shared/models/user.model";
 import { Router } from "@angular/router";
+import { UserService } from "src/app/services/user.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-table-user",
@@ -9,16 +11,56 @@ import { Router } from "@angular/router";
 })
 export class TableUserComponent implements OnInit {
   @Input() data: UserModel[];
+  @Input() edit: boolean;
+  @Output() onReload: EventEmitter<string>;
   public debug: boolean;
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router, private _userService: UserService) {}
 
   ngOnInit(): void {}
 
-  deleteItem(id: number) {}
+  deleteItem(id: number) {
+    const toast = Swal.mixin({
+      title: "SEGURO",
+      text: "no se podra revertir",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Si, borrar!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    });
+    toast.fire().then((result) => {
+      if (result.value) {
+        this._userService.delete(id).subscribe(
+          (resp) => {
+            const toast = Swal.mixin({
+              title: "BORRADO",
+              text: "cobro borrado",
+              icon: "success",
+            });
+            toast.fire();
+            this.onReload.emit("");
+            console.log(resp);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+      } else {
+        const toast = Swal.mixin({
+          title: "CANCELADO",
+          text: "tu cobro esta asalvo",
+          icon: "error",
+        });
+        toast.fire();
+      }
+    });
+  }
+
   selectItem(id: number) {
     this._router.navigate(["/usuario", id, "editar"]);
   }
+
   editItem(id: number) {
     this._router.navigate(["/usuario", id]);
   }
--- pagina-web/src/app/modules/document-type/document-type-list/document-type-list.component.ts
+++ pagina-web/src/app/modules/document-type/document-type-list/document-type-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DocumentTypeModel } from "src/app/shared/models/document-type.model";
+import { DocumentTypeService } from "src/app/services/document-type.service";
 
 @Component({
-  selector: 'app-document-type-list',
-  templateUrl: './document-type-list.component.html',
-  styleUrls: ['./document-type-list.component.scss']
+  selector: "app-document-type-list",
+  templateUrl: "./document-type-list.component.html",
+  styleUrls: ["./document-type-list.component.scss"],
 })
 export class DocumentTypeListComponent implements OnInit {
+  public documentType: DocumentTypeModel[];
 
-  constructor() { }
+  constructor(private _documentTypeService: DocumentTypeService) {}
 
   ngOnInit(): void {
+    this._documentTypeService.getList(1).subscribe(
+      (res) => {
+        this.documentType = res;
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
